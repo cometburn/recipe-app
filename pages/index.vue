fed80cf8aa52d7ca80ca5e5a001b4ae202b9fd2c
Jc Cometa <cometburn@gmail.com>
2020-12-02 21:38:05 +0800

    Edit layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,42 @@
 <template>
-  <div class="ra-login__page">
-    <h1>Login Form</h1>
-    <ra-login-card @submit="authenticate" />
-  </div>
+  <v-app>
+    <v-container fluid>
+      <v-row justify="center" no-gutters>
+        <v-col md="4" offset-md="3" cols="12">
+          <v-row>
+            <v-col class="text-center">
+              <h1>Login</h1>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <ra-login-card @submit="authenticate" />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="2"></v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import RaLoginCard from '~/components/organisms/ra-login-card/index.vue'
+import Vue from "vue";
+
+import RaLoginCard from "~/components/organisms/ra-login-card/index.vue";
 
 export default Vue.extend({
-  name: 'LoginPage',
-  components: {
-    RaLoginCard
-  },
-  methods: {
-    authenticate (): void {
-      // TODO
-    }
-  }
-})
+    name: "LoginPage",
+    components: {
+        RaLoginCard,
+    },
+    methods: {
+        authenticate(): void {
+            // TODO
+        },
+    },
+    data: () => ({
+        justify: false,
+    }),
+});
 </script>
